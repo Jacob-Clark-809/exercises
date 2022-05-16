f4cf42d239cd41c6bb9216cf22715c227121a678
Jacob Clark <jacobclark1198@gmail.com>
2022-05-16 13:43:30 +0000

    Methods

---
+++ ruby_basics/methods/ex3.rb
@@ -0,0 +1,9 @@
+def one
+  "Hello"
+end
+
+def two
+  "World"
+end
+
+puts one + " " + two
---
+++ ruby_basics/methods/ex6.rb
@@ -0,0 +1,8 @@
+daylight = [true, false].sample
+
+def time_of_day(daylight)
+  puts daylight ? "It's daytime!" : "It's nighttime!"
+end
+  
+time_of_day(daylight)
+  
